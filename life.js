7436dac9c62763b2099e1fe285c1a5550c68ba53
glptrst <giulio.pietroiusti@gmail.com>
2020-08-16 15:24:41 +0200

    Capitalize tool names.

--- life.js
+++ life.js
@@ -299,8 +299,9 @@
       this.select = elt("select", {
 	onchange: () => dispatch({ tool: this.select.value })
       }, ...Object.keys(tools).map(name => elt("option", {
-	selected: name == state.tool
-      }, name)));
+	//selected: name == state.tool,
+	value: name
+      }, name[0].toUpperCase() + name.slice(1))));
       this.dom = elt("label", null, "Tool: ", this.select);
     }
     syncState(state) { this.select.value = state.tool; }
